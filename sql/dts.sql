0bf35d75cca62d2f806698891dc28bbc46ebd361
RUFINO <aguilarufino@gmail.com>
2016-08-17 08:18:13 +0800

    improve query result

--- sql/dts.sql
+++ sql/dts.sql
@@ -8,7 +8,7 @@ ORDER BY title
 SELECT d.objid,
 d.docstate,
 d.din,
-d.parentid,
+dl.objid AS parentid,
 d.documenttypeid,
 d.title,
 d.description,
@@ -42,6 +42,7 @@ dto.address
 FROM document d
 INNER JOIN document_task dt ON dt.`refid` = d.`objid`
 INNER JOIN document_task_org dto ON dto.`taskid` = dt.`objid`
+LEFT JOIN document_link dl ON dl.`taskid` = dt.`objid`
 WHERE ${filter}
 ORDER BY d.title
 
@@ -56,11 +57,17 @@ WHERE UPPER("Entity_Name") LIKE $P{searchtext}
 OR UPPER("Entity_AcronymAbbreviation") LIKE $P{searchtext} 
 ORDER BY "Entity_Name"
 
+[getUserOrg]
+SELECT organizationid,orgname,orgcode FROM user_organization
+WHERE orgname LIKE $P{searchtext} 
+OR orgcode LIKE $P{searchtext} 
+GROUP BY organizationid ORDER BY orgname
+
 [getList]
 SELECT d.objid,
 d.docstate,
 d.din,
-d.parentid,
+dl.objid AS parentid,
 d.documenttypeid,
 d.title,
 d.description,
@@ -99,21 +106,15 @@ dtyp.code AS documenttype_code,
 dtyp.name AS documenttype_name,
 dtyp.description AS documenttype_description,
 dtyp.haschild AS documenttype_haschild,
-(SELECT dto.orgid
-FROM document_task t2 
-INNER JOIN document_task_org dto ON dto.taskid = t2.objid
-WHERE t2.lft < dt.lft AND t2.rgt > dt.rgt    
-ORDER BY t2.rgt-dt.rgt ASC
-LIMIT 1) AS senderorg
+ug2.organizationid AS senderorg
 FROM document d
 INNER JOIN user_organization ug ON ug.objid = d.`recordlog_createdbyuserid`
 INNER JOIN document_task dt ON dt.`refid` = d.`objid`
 INNER JOIN document_task_org dto ON dto.`taskid` = dt.`objid`
 INNER JOIN document_type dtyp ON dtyp.`objid` = d.`documenttypeid`
-WHERE (d.din LIKE $P{searchtext}
-OR d.title LIKE $P{searchtext}
-OR d.description LIKE $P{searchtext}
-OR d.tags LIKE $P{searchtext})
+INNER JOIN user_organization ug2 ON ug2.`objid` = dt.`actor_objid`
+LEFT JOIN document_link dl ON dl.`taskid` = dt.`objid`
+WHERE 1=1
 ${filter}
 ORDER BY dt.startdate
 
@@ -121,7 +122,7 @@ ORDER BY dt.startdate
 SELECT d.objid,
 d.docstate,
 d.din,
-d.parentid,
+dl.objid AS parentid,
 d.documenttypeid,
 d.title,
 d.description,
@@ -159,21 +160,24 @@ dto.address,
 dtyp.code AS documenttype_code,
 dtyp.name AS documenttype_name,
 dtyp.description AS documenttype_description,
-dtyp.haschild AS documenttype_haschild
+dtyp.haschild AS documenttype_haschild,
+ug2.organizationid AS senderorg
 FROM document d
 INNER JOIN user_organization ug ON ug.objid = d.`recordlog_createdbyuserid`
 INNER JOIN document_task dt ON dt.`refid` = d.`objid`
 INNER JOIN document_task_org dto ON dto.`taskid` = dt.`objid`
 INNER JOIN document_type dtyp ON dtyp.`objid` = d.`documenttypeid`
+INNER JOIN user_organization ug2 ON ug2.`objid` = dt.`actor_objid`
+LEFT JOIN document_link dl ON dl.`taskid` = dt.`objid`
 WHERE ${filter}
-AND (dt.enddate IS NULL OR dt.state IN ('archive','attached'))
+AND (dt.enddate IS NULL OR dt.state IN ('archive','attached','linked'))
 ORDER BY d.title
 
 [getDocumentbyBarcode]
 SELECT d.objid,
 d.docstate,
 d.din,
-d.parentid,
+dl.objid AS parentid,
 d.documenttypeid,
 d.title,
 d.description,
@@ -217,8 +221,62 @@ INNER JOIN user_organization ug ON ug.objid = d.`recordlog_createdbyuserid`
 INNER JOIN document_task dt ON dt.`refid` = d.`objid`
 INNER JOIN document_task_org dto ON dto.`taskid` = dt.`objid`
 INNER JOIN document_type dtyp ON dtyp.`objid` = d.`documenttypeid`
+LEFT JOIN document_link dl ON dl.`taskid` = dt.`objid`
 WHERE ${filter}
-AND (dt.enddate IS NULL OR dt.state IN ('archive','attached'))
+AND (dt.enddate IS NULL OR dt.state IN ('archive','attached','linked'))
+ORDER BY d.title, dt.startdate
+
+[getDocumentChild]
+SELECT d.objid,
+d.docstate,
+d.din,
+dl.`objid` AS parentid,
+d.documenttypeid,
+d.title,
+d.description,
+d.tags,
+d.author,
+d.recordlog_datecreated,
+d.recordlog_createdbyuserid,
+d.recordlog_createdbyuser,
+d.recordlog_dateoflastupdate,
+d.recordlog_lastupdatedbyuserid,
+d.recordlog_lastupdatedbyuser,
+ug.organizationid AS originorgid,
+ug.orgname AS originorgname,
+ug.orgcode AS originorgcode,
+dt.objid AS taskid,
+dt.refid,
+dt.parentprocessid,
+dt.state,
+dt.startdate,
+dt.assignee_objid,
+dt.assignee_name,
+dt.assignee_title,
+dt.enddate,
+dt.actor_objid,
+dt.actor_name,
+dt.actor_title,
+dt.message,
+dt.signature,
+dt.lft,
+dt.rgt,
+dto.orgid,
+dto.macaddress,
+dto.name,
+dto.address,
+dtyp.code AS documenttype_code,
+dtyp.name AS documenttype_name,
+dtyp.description AS documenttype_description,
+dtyp.haschild AS documenttype_haschild
+FROM document d
+INNER JOIN user_organization ug ON ug.objid = d.`recordlog_createdbyuserid`
+INNER JOIN document_task dt ON dt.`refid` = d.`objid`
+INNER JOIN document_task_org dto ON dto.`taskid` = dt.`objid`
+INNER JOIN document_type dtyp ON dtyp.`objid` = d.`documenttypeid`
+LEFT JOIN document_link dl ON dl.`taskid` = dt.`objid`
+WHERE ${filter}
+AND dt.state IN ('archive','attached','linked')
 ORDER BY d.title, dt.startdate
 
 [updateparent]
@@ -234,12 +292,12 @@ AND $P{sequence} BETWEEN startseries AND endseries
 AND orgid = $P{orgid}
 
 [findDocStatsByOrg]
-SELECT COUNT(*) AS TOTAL,
-       SUM(IF(dt.state = "outgoing",1,0)) AS OUTGOING,
-       SUM(IF(dt.state = "enroute",1,0)) AS ENROUTE,
-       SUM(IF(dt.state = "processing",1,0)) AS PROCESSING,
-       SUM(IF(dt.state = "archive",1,0)) AS ARCHIVE,
-       SUM(IF(dt.state = "attached",1,0)) AS ATTACHED
+SELECT COUNT(*) AS total,
+       SUM(IF(dt.state = "outgoing",1,0)) AS outgoing,
+       SUM(IF(dt.state = "enroute",1,0)) AS enroute,
+       SUM(IF(dt.state = "processing",1,0)) AS processing,
+       SUM(IF(dt.state = "archive",1,0)) AS archive,
+       SUM(IF(dt.state = "attached",1,0)) AS attached
 	
 FROM document d
 INNER JOIN user_organization ug ON ug.objid = d.`recordlog_createdbyuserid`
@@ -247,7 +305,42 @@ INNER JOIN document_task dt ON dt.`refid` = d.`objid`
 INNER JOIN document_task_org dto ON dto.`taskid` = dt.`objid`
 INNER JOIN document_type dtyp ON dtyp.`objid` = d.`documenttypeid`
 WHERE (dt.enddate IS NULL 
-OR dt.state IN ('attached','ARCHIVE','closed')) 
-AND dto.orgid = 'bacb54b6-912e-4167-bd9b-7b361e83a8e7'
+OR dt.state IN ('attached','archive','closed')) 
+AND dto.orgid = $P{userorgid}
 ORDER BY d.title, dt.startdate
+
+[getaftemp]
+SELECT * FROM af_temp
+
+[getdetails]
+SELECT c.`controlid` AS controlid,
+r.`objid` AS refid,
+r.`txnno` AS refno,
+'remittance' AS reftype,
+r.`remittancedate` AS refdate,
+r.`remittancedate` AS txndate,
+'REMITTANCE' AS txntype,
+NULL AS receivedstartseries,
+NULL AS receivedendseries,
+NULL AS beginstartseries,
+NULL AS beginendseries,
+MIN(c.`series`) AS issuedstartseries,
+MAX(c.`series`) AS issuedendseries,
+MAX(c.`series`)+1 AS endingstartseries,
+afi.`endseries` AS endingendseries,
+NULL AS cancelledstartseries,
+NULL AS cancelledendseries,
+0 AS qtyreceived,
+0 AS qtybegin,
+COUNT(c.`series`) AS qtyissued,
+afi.`endseries`- MAX(c.`series`) AS qtyending,
+0 AS qtycancelled,
+'REMITTANCE' AS remarks 
+FROM cashreceipt c
+INNER JOIN af_inventory afi ON afi.`objid` = c.`controlid`
+INNER JOIN remittance_cashreceipt rc ON rc.`objid` = c.`objid`
+INNER JOIN remittance r ON r.`objid` = rc.`remittanceid`
+WHERE c.controlid = $P{controlid}
+GROUP BY r.`objid`
+ORDER BY series;
 
